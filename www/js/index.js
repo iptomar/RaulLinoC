c65fe3f6217255d467595196a59cb7218340431c
Daniel Graça <danielgrascas@gmail.com>
2023-06-04 19:02:40 +0100

    Revert "Merge pull request #90 from iptomar/multi-language"
    This reverts commit 7c5d3bb7adee9cc325bd294edff0263ece1fd5b2, reversing
    changes made to 362f66ef7fa3f40dcab579f0b7b2d9dcd6d791ba.

--- www/js/index.js
+++ www/js/index.js
@@ -17,46 +17,26 @@
  * under the License.
  */
 
-/**
- * When the page is loaded, loads the data from the json files and sets the default language to portuguese
- */
-window.onload = async () => {
-    await fetchLocalization();
-    await fetchData();
-    getLang();
+// when page is loaded, loads home view
+window.onload = () => {
     changeView('home');
-    loadLanguageContent(lang);
 }
 
-//html5 webstorage
-let storage = window.localStorage;
-//holds the current language
-let lang;
-//holds the localization data from the json file
-let localization;
-//holds the Raul Lino data from the json file
-let data;
-//holds the view that is currently being displayed
+// holds the view that is currently being displayed
 let currView = "home";
-//holds map object
+// holds map object
 let map, gpsPosition;
 //bool to control current status of the user location marker
 let markerExists = false;
 let firstRun = true;
 
-//convert coordinates to leaflet object (Abrantes box corners in order to set map bounds)
-//these coordinates were acquired without any study (eye estimation)
+// convert coordinates to leaflet object (Abrantes box corners in order to set map bounds)
+// these coordinates were acquired without any study (eye estimation)
 const UPLEFTCORNER = L.latLng(39.510042, -8.296089);
 const DOWNRIGHTCORNER = L.latLng(39.401459, -8.050828);
 
-//use those coordinates to define the bounds of the map
+// use those coordinates to define the bounds of the map
 const bounds = L.latLngBounds(UPLEFTCORNER, DOWNRIGHTCORNER);
-
-
-//gets language from storage and sets it as the app language
-document.addEventListener('deviceready', () => {
-    lang = getLang();
-});
 
 //loads current user location into gpsPosition global variable
 function onLocationFound(e) {
@@ -75,33 +55,32 @@ function onLocationError() {
  * @param {*} position coordinates of the user's location
  */
 function onSuccess(position) {
-    
     //set current user location
     gpsPosition = position.coords;
 
 
     console.log("Starting map loading.")
-    //create the map with
+    // create the map with
     map = L.map('map', {
         center: [position.coords.latitude, position.coords.longitude],
         maxZoom: 18,
         minZoom: 12,
     }).setView([position.coords.latitude, position.coords.longitude], 12);
 
-    //create fullscreen control and add it to the map
+    // create fullscreen control and add it to the map
     L.control.fullscreen({
-        title: 'FullScreen Mode', //change the title of the button, default Full Screen
-        titleCancel: 'Exit FullScreen Mode', //change the title of the button when fullscreen is on, default Exit Full Screen
-        forceSeparateButton: true, //separate button from zoom buttons
-        forcePseudoFullscreen: true //force use of pseudo full screen, makes the fullscreen work incase the api fails
+        title: 'FullScreen Mode', // change the title of the button, default Full Screen
+        titleCancel: 'Exit FullScreen Mode', // change the title of the button when fullscreen is on, default Exit Full Screen
+        forceSeparateButton: true, // separate button from zoom buttons
+        forcePseudoFullscreen: true // force use of pseudo full screen, makes the fullscreen work incase the api fails
     }).addTo(map);
 
-    //sets max bounds
+    // sets max bounds
     map.setMaxBounds(bounds);
     //call onLocationFound when user location is found
     map.on('locationfound', onLocationFound);
 
-    //add the OpenStreetMap tiles (making the map usable)
+    // add the OpenStreetMap tiles (making the map usable)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
     //costum green marker    
@@ -125,34 +104,55 @@ function onSuccess(position) {
     });
 
     //variables to hold the markers that are going to change icons
-    let yellowMarkers = L.layerGroup();
-    let greenMarkers = L.layerGroup();
-    
-    //add the markers based on the each elements coordinates to the map
-    data[lang].forEach(element => {
-        //if the element belong to the yellow itinerary, add it to the yellowMarkers layer
-        if ([1,4,6,7,10,11,14,15,16].includes(element.id)){
-            yellowMarkers.addLayer(L.marker([element.coords[0], element.coords[1]], { icon: markerY })
-                .bindPopup('<a style="cursor:pointer;" onclick="pointsDescription(' + element.id + ');">' + element.title + '</a>'));
-            greenMarkers.addLayer(L.marker([element.coords[0], element.coords[1]], { icon: markerG })
-                .bindPopup('<a style="cursor:pointer;" onclick="pointsDescription(' + element.id + ');">' + element.title + '</a>'));
-        //if the element belongs to the green itinerary, add a green marker
-        }else{
-            L.marker([element.coords[0], element.coords[1]], { icon: markerG })
-                .addTo(map)
-                .bindPopup('<a style="cursor:pointer;" onclick="pointsDescription(' + element.id + ');">' + element.title + '</a>');
-        }
-    });
+    var yellowMarkers = L.layerGroup();
+    var greenMarkers = L.layerGroup();
 
-    //add the greenMarkers layer group to the map
-    greenMarkers.addTo(map);
-    
+    // specify popup options 
+    var customOptions = {
+        'maxWidth': '150',
+        'minWidth': '150',
+        'className' : 'custom'
+    };
+
+    // fetches data from json file and add the markers based on the each elemnts coordinates to the map
+    fetch("dados_raulLino.json")
+        .then(response => response.json())
+        .then(json => {
+            json.data.forEach(element => {
+                var customPopup = '<a class="cursor-pointer" onclick="pointsDescription(' + element.id + ');">'+
+                    '<div class="row d-flex align-items-center">'+
+                        '<div class="col no-padding text-left pr-2">'+
+                            element.title +
+                        '</div>'+
+                        '<div class="col-2 no-padding">'+
+                            '<img src="img/icons/ArrowTopRightOnSquare.svg" class="icon-popup">'+
+                        '</div>'+
+                    '</div>'+
+                '</a>';
+                //if the element belong to the yellow itinerary, add it to the yellowMarkers layer
+                if ([1,4,6,7,10,11,14,15,16].includes(element.id)){
+                    yellowMarkers.addLayer(L.marker([element.coords[0], element.coords[1]], { icon: markerY })
+                        .bindPopup(customPopup, customOptions));
+                    greenMarkers.addLayer(L.marker([element.coords[0], element.coords[1]], { icon: markerG })
+                        .bindPopup(customPopup, customOptions));
+                //if the element belongs to the green itinerary, add a green marker
+                }else{
+                    L.marker([element.coords[0], element.coords[1]], { icon: markerG })
+                        .addTo(map)
+                        .bindPopup(customPopup, customOptions);
+                }
+            });
+
+            //add the greenMarkers layer group to the map
+            greenMarkers.addTo(map);
+            
+        });
 
     //call the refresh function every 5 seconds
     setInterval(refreshUserMarker, 5000);
 
     //yellow itinerary line coordinates
-    let yellowItinerary = L.polyline([
+    var yellowItinerary = L.polyline([
         [39.463470, -8.201936], 
         [39.463956, -8.200545],
         [39.463898, -8.200425],
@@ -172,7 +172,7 @@ function onSuccess(position) {
     ], {color: 'yellow'});
 
     //green itinerary line coordinates
-    let greenItinerary = L.polyline([
+    var greenItinerary = L.polyline([
         [39.461363, -8.198468],
         [39.461824, -8.198045],
         [39.461453, -8.197270],
@@ -186,13 +186,13 @@ function onSuccess(position) {
     ], {color: 'green'});
 
     //adds the itineraries to the itinerary layer
-    let itineraryLayer = L.layerGroup([yellowItinerary, greenItinerary]);
+    var itineraryLayer = L.layerGroup([yellowItinerary, greenItinerary]);
 
     //boolean to check if the itineraries are being shown
-    let itinerariesShown = false;
+    var itinerariesShown = false;
 
     //icon for the show/hide itineraries button
-    let itineraryIcon = L.icon({
+    var itineraryIcon = L.icon({
         iconUrl: 'img\\icons\\mapa_itinerario.svg',
         iconSize: [50, 50],
         //icon allignment set to bottom mid corner of the icon
@@ -238,7 +238,7 @@ function onSuccess(position) {
  * @param {*} error 
  */
 function onError(error) {
-    swal(localization[lang].location.error);
+    swal("Erro.");
     console.log("error code:" + error.code);
     console.log("error message:" + error.message);
 }
@@ -273,56 +273,26 @@ function changeView(view) {
         document.getElementById(currView + "Line").classList.add('yellow-divisor');
     }
 
-    //if current view is map, loads map
+    // if current view is map, loads map
     if (currView == "mapPage") {
         
         askPermission();
 
         if (window.cordova) {
-            //Check if location is enabled using cordova.plugins.diagnostic.isLocationEnabled
+            // Check if location is enabled using cordova.plugins.diagnostic.isLocationEnabled
             cordova.plugins.diagnostic.isLocationEnabled(function (enabled) {
                 if (!enabled) {
-                    //Display a swal (SweetAlert) if location is disabled
-                    swal(localization[lang].location.deactivated);
+                    // Display a swal (SweetAlert) if location is disabled
+                    swal("Localização está desativada. Por favor ative-a para que a aplicação funcione corretamente");
                 }
             }, function (error) {
-                //Display an error swal if an error occurs
+                // Display an error swal if an error occurs
                 console.log("The following error occurred: " + error.message);
             });
         }
         map.invalidateSize();
         refreshUserMarker();
     }
-
-    //if current view is settings, loads settings
-    if (currView == "settings") {
-        populateLanguageSelector();
-    }
-}
-
-//populates the selector with the available languages
-let populateLanguageSelector = () => {
-    const languageSelector = document.getElementById("language-select");
-    languageSelector.innerHTML = "";
-
-    //gets all the languages from the localization file
-    const languages = Object.keys(localization);
-
-    //adds all the languages to the selector
-    for (let i = 0; i < languages.length; i++) {
-        let option = document.createElement("option");
-        option.text = option.value = languages[i];
-        languageSelector.add(option);
-    }
-
-    //sets the current language as the selected option
-    languageSelector.value = getLang();
-    
-    //adds an event listener to the selector to change the language
-    languageSelector.addEventListener("change", () => {
-        setLang(languageSelector.value);
-        languageSelector.value = getLang();
-    });
 }
 
 
@@ -333,12 +303,12 @@ function refreshUserMarker() {
     //update user coords every time the function is called
     navigator.geolocation.watchPosition(onLocationFound, onLocationError);
     //creates the user icon to be added to the map
-    let userIcon = L.icon({
+    var userIcon = L.icon({
         iconUrl: 'img/icons/userIcon.svg',
 
-        iconSize: [20, 40], //size of the icon
-        iconAnchor: [10, 40], //point of the icon which will correspond to marker's location
-        popupAnchor: [0, -40] //point from which the popup should open relative to the iconAnchor
+        iconSize: [20, 40], // size of the icon
+        iconAnchor: [10, 40], // point of the icon which will correspond to marker's location
+        popupAnchor: [0, -40] // point from which the popup should open relative to the iconAnchor
     });
 
     //if the user is within the defined bounds, adds or updates his current location into a marker, otherwise removes it if it exists
@@ -366,119 +336,32 @@ function refreshUserMarker() {
  * @param {*} id id of the point
  */
 function pointsDescription(id) {
-    let auxDesc = '', auxImg = '';
+    var auxDesc = '', auxImg = '';
+    fetch("dados_raulLino.json")
+        .then(response => response.json())
+        .then(json => {
+            auxDesc += '<div>';
+            auxDesc += '<h1 class="display-6">' + json.data[id].title + '</h1><br />';
+            auxDesc += '<p>' + json.data[id].info + '</p>';
+            auxDesc += '<p>Ano: ' + json.data[id].year + '</p>';
+            auxDesc += '<p>Morada: ' + json.data[id].location + '</p>';
+            auxDesc += '<p>Tipo de Edifício: ' + json.data[id].type + '</p>';
+            auxDesc += '</div>';
 
-    auxDesc += '<div>';
-    auxDesc += '<h1 class="display-6">' + data[lang][id].title + '</h1><br />';
-    auxDesc += '<p>' + data[lang][id].info + '</p>';
-    auxDesc += '<p>' + localization[lang].year + ': ' + data[lang][id].year + '</p>';
-    auxDesc += '<p>' + localization[lang].address + ': ' + data[lang][id].location + '</p>';
-    auxDesc += '<p>' + localization[lang]['type-of-building'] + ': ' + data[lang][id].type + '</p>';
-    auxDesc += '</div>';
+            for (var i = 0; i < json.data[id].images.length; i++) {
+                var element = json.data[id].images[i];
+                if (i === 0) {
+                    auxImg += '<div class="carousel-item active">';
+                } else {
+                    auxImg += '<div class="carousel-item">';
+                }
+                auxImg += '<img class="d-block w-100 car-img" src="' + element + '" alt="Slide ' + (i + 1) + '">' + '</div>';
+            }
 
-    for (let i = 0; i < data[lang][id].images.length; i++) {
-        let element = data[lang][id].images[i];
-        if (i === 0) {
-            auxImg += '<div class="carousel-item active">';
-        } else {
-            auxImg += '<div class="carousel-item">';
-        }
-        auxImg += '<img class="d-block w-100 car-img" src="' + element + '" alt="Slide ' + (i + 1) + '">' + '</div>';
-    }
-
-    document.getElementById("iterPDesc").innerHTML = auxDesc;
-    document.getElementById("iterPImg").innerHTML = auxImg;
-
+            document.getElementById("iterPDesc").innerHTML = auxDesc;
+            document.getElementById("iterPImg").innerHTML = auxImg;
+        });
     changeView("desc");
-}
-
-/**
- * Fetches and load the localization data from the json file
- */
-let fetchLocalization = () => {
-    return fetch('./localization.json')
-        .then(response => response.json())
-        .then(response => {
-            //assign the data to the global variable
-            localization = response;
-            return localization;
-        })
-        .catch(error => {
-            //handle any errors
-            console.error(error);
-        });
-}
-
-/**
- * Fetches and load the Raul Lino data from the json file
- */
-let fetchData = () => {
-    return fetch('./dados_raulLino.json')
-        .then(response => response.json())
-        .then(response => {
-            //assign the data to the global variable
-            data = response;
-            return data;
-        })
-        .catch(error => {
-            //handle any errors
-            console.error(error);
-        });
-}
-
-/**
- * Loads the content of all static pages based on the current language
- */
-let loadLanguageContent = (lang) => {
-    //resets all paragraphs, so it doesn't append the new ones to the old ones
-    document.getElementById("paragraphs-home").innerHTML = "";
-    document.getElementById("paragraphs-bio").innerHTML = "";
-
-    //main elements
-    document.getElementById("main-title").innerHTML = localization[lang]["main-title"];
-    document.getElementById("raul-lino-title").innerHTML = localization[lang].title;
-
-    //home page
-    document.getElementById("page-title-home").innerHTML = localization[lang].home.title;
-    localization[lang].home.paragraphs.forEach(paragraph => {
-        document.getElementById("paragraphs-home").innerHTML += "<p>" + paragraph + "</p>";
-    });
-
-    //bio page
-    document.getElementById("page-title-bio").innerHTML = localization[lang].bio.title;
-    localization[lang].bio.paragraphs.forEach(paragraph => {
-        document.getElementById("paragraphs-bio").innerHTML += "<p>" + paragraph + "</p>";
-    });
-
-    //bio page
-    document.getElementById("page-title-map").innerHTML = localization[lang].map.title;
-
-    //settings page
-    document.getElementById("page-title-settings").innerHTML = localization[lang].settings.title;
-    document.getElementById("language-label").innerHTML = localization[lang].settings.language;
-
-    //carousel
-    document.getElementById("previous").innerHTML = localization[lang].map.previous;
-    document.getElementById("next").innerHTML = localization[lang].map.next;
-}
-
-/**
- * sets the language in the local storage
- * @param {string} lang 
- */
-let setLang = (newLang) => {
-    lang = newLang;
-    storage.setItem("lang", lang);
-    loadLanguageContent(lang);
-}
-
-/**
- * 
- * @returns the chosen language stored in the local storage
- * if there is no language stored, returns the default language (pt-PT)
- */
-let getLang = () => {
-    return storage.getItem("lang") ?? setLang("pt-PT");
 }
 
 
